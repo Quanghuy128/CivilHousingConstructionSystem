19f2316f7fadfc9be43fb93718977cf15615b1be
Huy dang hoc code <60337562+Quanghuy128@users.noreply.github.com>
2024-02-25 19:23:22 +0700

    [+] add postgres image and update app infrastructure

---
+++ CHC.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CHC.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CHC.Presentation/Configuration/ConfigureDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace CHC.Presentation.Configuration
+{
+    public static class ConfigureDbContext
+    {
+        public static IServiceCollection AddDbContext(this IServiceCollection services)
+        {
+            return services.AddDbContext();
+        }
+    }
+}
