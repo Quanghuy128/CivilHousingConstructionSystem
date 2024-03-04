278659994069a3b124ddee34e5e7112b57788f3e
Huy dang hoc code <60337562+Quanghuy128@users.noreply.github.com>
2024-03-04 18:19:10 +0700

    [+] add templates

--- CHC.Presentation/Pages/Login.cshtml.cs
+++ CHC.Presentation/Pages/Login.cshtml.cs
@@ -1,6 +1,8 @@
 using CHC.Application.Service;
+using CHC.Domain.Common;
 using CHC.Domain.Dtos;
 using CHC.Domain.Dtos.Account;
+using CHC.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -13,7 +15,10 @@ namespace CHC.Presentation.Pages
         private readonly IAccountService accountService;
         private readonly IHttpContextAccessor httpContextAccessor;
 
-        public LoginModel(IConfiguration configuration, IAccountService accountService, IHttpContextAccessor httpContextAccessor)
+        public LoginModel(
+            IConfiguration configuration, 
+            IAccountService accountService, 
+            IHttpContextAccessor httpContextAccessor)
         {
             this.configuration = configuration;
             this.accountService = accountService;
@@ -26,7 +31,7 @@ namespace CHC.Presentation.Pages
         }
 
         [BindProperty(SupportsGet = true)]
-        public AccountDto Account { get; set; } = default!;
+        public CreateAccountRequest Account { get; set; } = default!;
         public string ErrorMessage { get; set; } = string.Empty;
 
         public async Task<IActionResult> OnPostAsync()
@@ -34,6 +39,20 @@ namespace CHC.Presentation.Pages
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+
+            if(Account.Username == AppConfig.Admin.Username && Account.Password == AppConfig.Admin.Password)
+            {
+                string customerJson = JsonConvert.SerializeObject(new SessionUser
+                {
+                    Username = Account.Username,
+                    DisplayName = Account.Username,
+                    Email = Account.Username,
+                    Role = RoleType.Admin,
+                });
+                httpContextAccessor.HttpContext!.Session.Clear();
+                httpContextAccessor.HttpContext!.Session.SetString("CurrentUser", customerJson);
+                return Redirect("/AccountView/Index");
             }
 
             AccountDto account = await accountService.Login(Account.Username, Account.Password);
@@ -46,7 +65,9 @@ namespace CHC.Presentation.Pages
             {
                 string customerJson = JsonConvert.SerializeObject(new SessionUser 
                 { 
+                    Id = account.Id,
                     Username = account.Username,
+                    DisplayName= account.FullName,
                     Email = account.Email,
                     Role = account.Role,
                 });
